f0152d803f4fbc91e87c6a17b5f3ea075da3329c
afreen505 <afreen.fathimaa9876@gmail.com>
2021-08-31 13:54:54 +0530

    Added :Executed program

---
+++ src/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,12 @@
+package moodanalyser;
+
+public class MoodAnalyser {
+
+    public String analyseMood(String message) {
+        if (message.contains("sad")) {
+            return "SAD";
+        } else {
+            return "HAPPY";
+        }
+    }
+}
---
+++ src/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,27 @@
+package moodanalyser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+    public class MoodAnalyserTest {
+
+        MoodAnalyser moodanalyser = null;
+
+        @Before
+        public void seUp() {
+            moodanalyser = new MoodAnalyser();
+        }
+
+        @Test
+        public void givenMessageIsSadMood_shouldReturn_Sad() {
+            String mood = moodanalyser.analyseMood("I am in sad mood");
+            Assert.assertEquals("SAD", mood);
+        }
+
+        @Test
+        public void givenMessageIsAny_shouldReturn_Happy() {
+            String mood = moodanalyser.analyseMood("I am in any mood");
+            Assert.assertEquals("HAPPY", mood);
+        }
+
+    }
