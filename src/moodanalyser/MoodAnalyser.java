62e8c1a87fa12f9379021ce7c4f5cdb2a0aa3555
afreen505 <afreen.fathimaa9876@gmail.com>
2021-08-31 14:16:46 +0530

    Modified: UC1 executed successfully

--- src/moodanalyser/MoodAnalyser.java
+++ src/moodanalyser/MoodAnalyser.java
@@ -1,12 +1,40 @@
 package moodanalyser;
 
 public class MoodAnalyser {
+    public String message;
 
+    /**
+     * analyseMood is a method to check if sad message then return sad .
+     * if happy message then return happy
+     */
     public String analyseMood(String message) {
-        if (message.contains("sad")) {
+        if (message.contains("Sad"))
             return "SAD";
-        } else {
-            return "HAPPPY";
+        return "HAPPY";
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Default constructor
+     */
+    public MoodAnalyser() {
+        this.message = message;
+    }
+
+    /**
+     * exceptionAnalyseMood is a function check if message contain sad then return sad.
+     * if contain happy then return happy if contain null then also return happy
+     */
+    public String exceptionAnalyseMood() {
+        try {
+            if (message.contains("sad"))
+                return "SAD";
+            return "HAPPY";
+        } catch (NullPointerException e) {
+            return "HAPPY";
         }
     }
 }
