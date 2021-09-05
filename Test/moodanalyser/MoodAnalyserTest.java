f27e7cde95d1e6a482348eebf106db518e977fe1
afreen505 <afreen.fathimaa9876@gmail.com>
2021-09-05 21:48:02 +0530

    Modified:Refacored the usecase and executed successfully

--- Test/moodanalyser/MoodAnalyserTest.java
+++ Test/moodanalyser/MoodAnalyserTest.java
@@ -8,45 +8,15 @@ public class MoodAnalyserTest {
     @Test
     public void givenMessageIsSadMood_shouldReturn_Sad() {
         MoodAnalyser moodanalyser = new MoodAnalyser("I am in sad mood");
-        String mood = null;
-        try {
-            mood = moodanalyser.analyseMood();
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
-        Assert.assertEquals("SAD", mood);
+        // String mood = moodanalyser.analyseMood();
+        Assert.assertEquals("SAD", moodanalyser.analyseMood());
     }
 
     @Test
     public void givenMessageNotSad_shouldReturn_Happy() {
         MoodAnalyser moodanalyser = new MoodAnalyser("I am in any mood");
-        String mood = null;
-        try {
-            mood = moodanalyser.analyseMood();
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
-        Assert.assertEquals("HAPPY", mood);
-    }
-
-    @Test
-    public void givenMessageIsNullMood_should_ThrowException() {
-        MoodAnalyser moodanalyser = new MoodAnalyser(null);
-        try {
-            moodanalyser.analyseMood();
-        } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.expectedType.Null_EXCEPTION, e.type);
-        }
-    }
-
-    @Test
-    public void givenMessageIsEmpty_should_ThrowException() {
-        MoodAnalyser moodanalyser = new MoodAnalyser("");
-        try {
-            moodanalyser.analyseMood();
-        } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.expectedType.EMPTY_EXCEPTION, e.type);
-        }
+        // String mood = moodanalyser.analyseMood();
+        Assert.assertEquals("HAPPY", moodanalyser.analyseMood());
     }
 
 }
--- main/moodanalyser/MoodAnalyser.java
+++ main/moodanalyser/MoodAnalyser.java
@@ -2,28 +2,20 @@ package moodanalyser;
 
 public class MoodAnalyser {
     private String message;
+
+    public MoodAnalyser() {
+        super();
+    }
+
     public MoodAnalyser(String message) {
         this.message = message;
     }
 
-
-    /**
-     * If message is sad it returns SAD else if message is empty it throws custom exception "EMPTY_EXCEPTION"
-     * Else it returns HAPPY
-     * @return mood
-     * @throws MoodAnalysisException
-     */
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-            if(message.isEmpty()) {
-                throw new MoodAnalysisException(MoodAnalysisException.expectedType.EMPTY_EXCEPTION, "Please enter a proper message");
-            }
-            else if (message.contains("sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        } catch (NullPointerException e) {
-            throw new MoodAnalysisException(MoodAnalysisException.expectedType.Null_EXCEPTION, "Please enter a proper message");
+    public String analyseMood() {
+        if (message.contains("sad")) {
+            return "SAD";
+        } else {
+            return "HAPPY";
         }
     }
 }
