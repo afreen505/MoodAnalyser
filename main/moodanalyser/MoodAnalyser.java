e87fee3362c9bfe3b7911c826baf2e48130b94ea
afreen505 <afreen.fathimaa9876@gmail.com>
2021-09-05 21:44:25 +0530

    Modified:Handle Custom Exception if user entered invalid mood

--- main/moodanalyser/MoodAnalyser.java
+++ main/moodanalyser/MoodAnalyser.java
@@ -2,23 +2,28 @@ package moodanalyser;
 
 public class MoodAnalyser {
     private String message;
-
     public MoodAnalyser(String message) {
         this.message = message;
     }
 
+
     /**
-     * If message contains "sad" it returns SAD else it returns HAPPY. If message is
-     * Null it returns HAPPY
+     * If message is sad it returns SAD else if message is empty it throws custom exception "EMPTY_EXCEPTION"
+     * Else it returns HAPPY
+     * @return mood
+     * @throws MoodAnalysisException
      */
-    public String analyseMood() {
+    public String analyseMood() throws MoodAnalysisException {
         try {
-            if (message.contains("sad"))
+            if(message.isEmpty()) {
+                throw new MoodAnalysisException(MoodAnalysisException.expectedType.EMPTY_EXCEPTION, "Please enter a proper message");
+            }
+            else if (message.contains("sad"))
                 return "SAD";
             else
                 return "HAPPY";
         } catch (NullPointerException e) {
-            return "HAPPY";
+            throw new MoodAnalysisException(MoodAnalysisException.expectedType.Null_EXCEPTION, "Please enter a proper message");
         }
     }
 }
--- main/moodanalyser/MoodAnalysisException.java
+++ main/moodanalyser/MoodAnalysisException.java
@@ -1,2 +1,16 @@
-package moodanalyser;public class MoodAnalysisException {
+package moodanalyser;
+
+public class MoodAnalysisException extends Exception {
+    enum expectedType {
+        Null_EXCEPTION, EMPTY_EXCEPTION;
+    }
+
+    expectedType type;
+
+    public MoodAnalysisException(expectedType type, String message) {
+        super(message);
+        this.type = type;
+    }
+
 }
+
